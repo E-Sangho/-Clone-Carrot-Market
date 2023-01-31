e4db5608a8a9e486cc8cd752b93734a73757a602
Sangho <istrangeho@gmail.com>
2023-01-31 21:19:56 +0900

    finished to make Questions and Ask

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -17,7 +17,7 @@ interface AnswerWithUser extends Answer {
 interface PostWithUser extends Post {
 	user: User;
 	_count: {
-		answer: number;
+		answers: number;
 		wondering: number;
 	};
 	answers: AnswerWithUser[];
@@ -78,8 +78,9 @@ const CommunityPostDetail: NextPage = () => {
 	useEffect(() => {
 		if (answerData && answerData.ok) {
 			reset();
+			mutate();
 		}
-	}, [answerData, reset]);
+	}, [answerData, reset, mutate]);
 	return (
 		<Layout canGoBack>
 			<div>
@@ -143,7 +144,7 @@ const CommunityPostDetail: NextPage = () => {
 									d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
 								></path>
 							</svg>
-							<span>답변 {data?.post._count.answer}</span>
+							<span>답변 {data?.post?._count.answers}</span>
 						</span>
 					</div>
 				</div>
